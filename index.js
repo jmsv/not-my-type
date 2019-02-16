900df1159c6c6a5e860984923a17f6ec93366044
aduxbury0 <duxburya@uni.coventry.ac.uk>
2019-02-16 23:59:54 +0000

    network basics

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const path = require('path')
 const bodyParser = require('body-parser')
+const network = require('./modules/neuralNetwork')
 
 const app = express()
 
@@ -9,13 +10,17 @@ const routeDetect = require('./routes/detect')
 app.use(express.static(path.join(__dirname, 'client/build')))
 app.use(bodyParser.json())
 
-app.use('/api/detect', routeDetect, (req, res) => res.sendStatus(401))
+app.use('/api/detect', routeDetect)
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '/client/build/index.html'))
 })
 
 const port = process.env.PORT || 5000
-app.listen(port)
+app.listen(port, () => {
+  console.log(`listening on ${port}`)
+  let trainedNetwork = network.startNetwork();
+  
+});
 
-console.log(`listening on ${port}`)
+
