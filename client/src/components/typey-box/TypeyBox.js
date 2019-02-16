2b0efd13466e524d5110b7508f05b234c82ba0f2
James Vickery <dev@jamesvickery.net>
2019-02-16 23:58:42 +0000

    human training ui

--- client/src/components/typey-box/TypeyBox.js
+++ client/src/components/typey-box/TypeyBox.js
@@ -1,21 +1,118 @@
 import React, { Component } from 'react'
-import TextField from '@material-ui/core/TextField'
+import { TextField, LinearProgress, Paper, Fab } from '@material-ui/core'
+
+import { Save } from '@material-ui/icons'
+
+import downloadFile from 'js-file-download'
+
+import './TypeyBox.css'
+
+const mashTargetLength = 100
 
 export default class TaskList extends Component {
+  state = {
+    events: [],
+    done: false,
+    startTime: null,
+    mashVal: '',
+    eventsCollection: []
+  }
+
+  getEventTime = () => {
+    if (!this.state.startTime) {
+      this.setState({ startTime: new Date() })
+      return 0
+    }
+
+    return (new Date()) - this.state.startTime
+  }
+
+  recordEvent = event => {
+    const time = this.getEventTime()
+
+    const userEvent = {
+      key: event.key.toLowerCase(),
+      type: event.type,
+      time
+    }
+
+    if (userEvent.key.length > 1) return
+
+    this.setState({ events: [...this.state.events, userEvent] }, () => {
+      if (this.state.events.length >= mashTargetLength) {
+        this.finishedMash()
+      }
+    })
+  }
+
+  finishedMash = () => {
+    this.setState({
+      done: true,
+      mashVal: ''
+    })
+
+    this.setState({ eventsCollection: [...this.state.eventsCollection, { events: this.state.events }] }, () => {
+      this.setState({ events: [] })
+    })
+
+    setTimeout(() => {
+      this.setState({ done: false }, () => {
+        this.mashInput.focus()
+      })
+    }, 1000)
+  }
+
+  handleChange = event => {
+    this.setState({ [event.target.name]: event.target.value })
+  }
+
+  mashProgress = () => (this.state.events.length * 110) / mashTargetLength
+
+  downloadTrainingData = () => {
+    const json = JSON.stringify(this.state.eventsCollection, null, 2)
+    downloadFile(json, 'mash-training-data.json')
+  }
+
   render () {
     return (
       <div>
         <TextField
-          label=''
-          placeholder='mash me'
-          helperText=''
+          name="mashVal"
+          inputRef={input => this.mashInput = input}
+          autoFocus
           fullWidth
+          value={this.state.mashVal}
+          onChange={this.handleChange}
+          style={{ backgroundColor: this.state.done ? '#98ed71' : 'white' }}
+          label=''
+          placeholder=''
+          helperText=''
+          disabled={this.state.done}
           margin='normal'
           variant='outlined'
-          InputLabelProps={{
-            shrink: true
-          }}
+          onKeyDown={this.recordEvent}
+          onKeyUp={this.recordEvent}
         />
+
+        <LinearProgress color="primary" variant="determinate" value={this.mashProgress()} />
+
+        <div style={{textAlign: 'center', margin: 16}}>
+          mashes recorded: {this.state.eventsCollection.length}
+        </div>
+
+        <div style={{ textAlign: 'center' }}>
+          {this.state.eventsCollection.length ? (
+            <Fab
+              variant="extended"
+              color="primary"
+              onClick={this.downloadTrainingData}
+              style={{margin: 16}} >
+
+              <Save />&nbsp;Save Training Data
+            </Fab>
+          ) : ''}
+
+        </div>
       </div>
     )
   }
