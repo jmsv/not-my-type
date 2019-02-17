9ba0004c65c732e169a4974b8c76ab677d59854b
James Vickery <dev@jamesvickery.net>
2019-02-17 00:33:43 +0000

    fix a load of formatting lmao

--- client/src/components/typey-box/TypeyBox.js
+++ client/src/components/typey-box/TypeyBox.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { TextField, LinearProgress, Paper, Fab } from '@material-ui/core'
+import { TextField, LinearProgress, Fab } from '@material-ui/core'
 
 import { Save } from '@material-ui/icons'
 
--- index.js
+++ index.js
@@ -19,8 +19,5 @@ app.get('*', (req, res) => {
 const port = process.env.PORT || 5000
 app.listen(port, () => {
   console.log(`listening on ${port}`)
-  let trainedNetwork = network.startNetwork();
-  
-});
-
-
+  let trainedNetwork = network.startNetwork()
+})
